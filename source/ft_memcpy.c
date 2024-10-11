676fecd7f24873141b87df681ae2def0896083b3
ayoubouahidi <ouahidi.ayoub2002@gmail.com>
2024-10-11 01:02:00 +0100

    to be continue ...

--- source/ft_memcpy.c
+++ source/ft_memcpy.c
@@ -1,41 +1,37 @@
 #include <stdio.h>
 #include <string.h>
+#include "libft.h"
 
-void ft_memcpy(char *dest, char *src, unsigned int n)
+void ft_memcpy(void *dest, const void *src, size_t numByte)
 {
   int i;
+  char *newDest = (unsigned char *)dest;
+  const char * newSrc = (const unsigned char *)src;
 
   i = 0;
-  while (i != n)
+  while (i < numByte)
   {
-    dest[i] = src[i]; 
+    newDest[i] = newSrc[i]; 
     i++;
   }
-  // dest[i] = '\0';
+  if (i < numByte)
+  {
+    newDest[i] = '\0';
+  }
 }
 
 
 int main()
 {
-  // char src[20] = "ayoub ouahidi";
+ 
+  char src[] = "Ayoub Ouahidi";
+  char dest_s[] = "";
+  char dest_a[] = "";
 
-  char dest[20] ;
-  char src_2[20] = "ayoub ouahidi";
-  char dest_2[20] ;
-
-  char src_3[20] =  "ayoub ouahidi";
-  char dest_3[20] ;
-
-  char src_4[20] = "ouahidi ayoub";
-  char dest_4[20] = "0123456789";
-
-  char src[20] = "ouahidi ayoub";
-  char dest_s[20] = "0123456789";
-  char dest_a[20] = "0123456789";
-
-  // memcpy(dest_4, src_4, 2);
-  // // dest[2] = '\0';
-  // printf("string's function : %s\n", dest);
+  memcpy(src + 2, src, 4);
+  ft_memcpy(src + 2,src, 4);
+  printf("string's function : %s\n", src);
+  printf("ayoub's function : %s\n", src);
 
   // ft_memcpy(dest_4, src_4, 2);
   // printf("ayoub's function : %s\n", dest_4);
@@ -43,8 +39,8 @@ int main()
   // ft_memcpy(dest_3, src_3, 2);
   // printf("ayoub's function : %s\n", dest_3);
 
-  memcpy(dest_s, src, 4);
-  printf("string's function : %s\n", dest_s);
-  ft_memcpy(dest_a, src, 4);
-  printf("string's function : %s\n", dest_a);
+  // memcpy(dest_s, src, 4);
+  // printf("string's function : %s\n", dest_s);
+  // ft_memcpy(dest_a, src, 4);
+  // printf("string's function : %s\n", dest_a);
 }
--- source/ft_memmove.c
+++ source/ft_memmove.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 
-int ft_memmove(char *dest, char *src, size_t n)
+int ft_memmove(void *dest, const void *src, size_t numByte)
 {
   int i;
-
+  char *newdest = (unsigned char *)dest;
+  const char *newSrc =(const unsigned char *)src;
+  
   i = 0;
-  while (i != n)
+  while (i < numByte)
   {
-    dest[i] = src[i];
-    i++;
+    
   }
+
 }
 
 int main()
 {
-  char src[20] = "ouahidi ayoub";
-  char dest_s[20] = "0123456789";
-  char dest_a[20] = "0123456789";
+  char src[20]  = "";
+  char dest_s[20] = "";
+  char dest_a[20];
 
-  memmove(dest_s, src, 4);
+  memmove(dest_s, src, 6);
   printf("string's function : %s\n", dest_s);
-  ft_memmove(dest_a, src, 4);
-  printf("string's function : %s\n", dest_a);
+  // ft_memmove(dest_a, src, 4);
+  // printf("string's function : %s\n", dest_a);
 }
