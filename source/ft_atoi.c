dafa2c749f5637a8ba4fcf2b379a4446f41d9921
Ayoub ouahidi <ouahidi.ayoub@gmail.com>
2024-10-23 15:17:04 +0100

    atoi push

--- source/ft_atoi.c
+++ source/ft_atoi.c
@@ -1,47 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayouahid <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/22 22:05:14 by ayouahid          #+#    #+#             */
+/*   Updated: 2024/10/22 22:22:02 by ayouahid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <string.h>
 #include "libft.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-// void ft_putchar(char c){
-//   write(1, c, 1);
-// }
-
-int ft_atoi(char *str){
-  int i;
-  int cmpt;
-  int result;
-  int cmpmoins;
-
-  cmpmoins = 0;
-  result = 0;
-  cmpt = 0;
-  i = 0;
-  while (str[i] == 32 || str[i] != '\0')
-  {
-    i++;
-  }
-  while (str[i] == '-' && str[i] == '+')
-  {
-   cmpt++; 
-  }
-  
-  while (str[i] >= 48 && str[i] <= 57)
-  {
-    result += str[i] * 10;
-  }
-  if(cmpt > 1){
-     return 0; 
-  }
-  if (cmpmoins == 1){
-    return result * -1;
-  }
-  return result;
+int	fornotminette(int result, int cmpt, int cmpmoins)
+{
+	if (cmpt > 1)
+		return (0);
+	if (cmpmoins == 1)
+		return (result * -1);
+	return (result);
 }
 
-int main(){
-  printf("ayoub's function : %d\n", atoi("-19ayosd131ub"));
-  printf("string's function : %d\n", atoi("-19ouahi51di"));
-  return 0;
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	cmpt;
+	int	result;
+	int	cmpmoins;
+
+	cmpmoins = 0;
+	result = 0;
+	cmpt = 0;
+	i = 0;
+	while (str[i] == 32 && str[i] != '\0')
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		cmpt++;
+		if (str[i] == '-')
+			cmpmoins++;
+		i++;
+	}
+	while (str[i] >= 48 && str[i] <= 57)
+	{
+		result = result * 10 + (str[i] - 48);
+		i++;
+	}
+	fornotminette(result, cmpt, cmpmoins);
 }
