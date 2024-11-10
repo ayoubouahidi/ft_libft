2877b432a0eb34218612a1a35edb0692442aa2ed
ayoubouahidi <ouahidi.ayoub2002@gmail.com>
2024-11-10 19:01:50 +0100

    bonuns _2

--- source/ft_lstadd_back_bonus.c
+++ source/ft_lstadd_back_bonus.c
@@ -17,7 +17,8 @@ void ft_lstadd_back(t_list **lst, t_list *new)
 {
     if (!lst || !new)
         return;
-    t_list *tmp = *lst;
+    t_list *tmp;
+    tmp = *lst;
     while (tmp->next != NULL)
     {
         tmp = tmp->next;
--- source/ft_lstclear_bonus.c
+++ source/ft_lstclear_bonus.c
@@ -11,8 +11,47 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 void ft_lstclear(t_list **lst, void (*del)(void *))
 {
-    
+    t_list *tmp;
+
+    while (*lst != NULL)
+    {
+        tmp = (*lst)->next;
+        del((*lst)->content);
+        free(*lst);
+        *lst = tmp;        
+    }   
 }
+void delete(void *a)
+{
+    free(a);
+}
+int main()
+{
+    t_list *node1;
+    t_list *node2;
+    t_list *node3;
+    int i = 1;
+    int i1 = 2;
+    int i2  = 3;
+    node1 = ft_lstnew(malloc(sizeof(int)));
+    node2 = ft_lstnew(malloc(sizeof(int)));
+    node3 = ft_lstnew(malloc(sizeof(int)));
+    *(int *)node1->content = i;
+    *(int *)node2->content = i1; 
+    *(int *)node3->content = i2;
+    ft_lstadd_back(&node1, node2);
+    ft_lstadd_back(&node1, node3);
+    t_list *head = node1;
+    ft_lstclear(&node3, delete);
+    while (node1)
+    {
+        
+        printf("%d\n", *(int *)node1->content);
+        node1 = node1->next;
+    }   
+}//exit(0);
